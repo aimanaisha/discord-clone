f443a3bf19e68990d1de372dd3aa867ea577ac28
aiman <aimanaisha851@gmail.com>
2022-02-10 12:31:47 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        //headfont: ['nunito', 'sans-serif'],
+        //'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+      },
+      colors:{
+        col1: '#404eed'
+      },
+    },
+  },
+  plugins: [],
+}
