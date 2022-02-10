60314d4d3c121f7ec24aca4247f4b02e9bf8c864
aiman <aimanaisha851@gmail.com>
2022-02-10 16:16:52 +0530

    added bottom part

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        //headfont: ['nunito', 'sans-serif'],
+        headfont2: ['Rammetto One'],
         //'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
       },
       colors:{
